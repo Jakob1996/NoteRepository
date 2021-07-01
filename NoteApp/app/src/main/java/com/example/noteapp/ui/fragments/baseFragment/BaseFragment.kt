c0cb5663260be46d42071be7d6eead51d13f6089
jakub.miftadinow <jakub.miftadinow@proexe.eu>
2021-07-01 23:04:16 +0200

    Add base fragment

---
+++ NoteApp/app/src/main/java/com/example/noteapp/ui/fragments/baseFragment/BaseFragment.kt
@@ -0,0 +1,6 @@
+package com.example.noteapp.ui.fragments.baseFragment
+
+import androidx.fragment.app.Fragment
+import com.example.noteapp.navigation.Navigation
+
+abstract class BaseFragment:Fragment(), Navigation
